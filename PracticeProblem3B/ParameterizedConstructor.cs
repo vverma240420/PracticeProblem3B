bd9a8917cc67f2202e7b11747d808e45c25b9fb7
Vivek Verma <vverma240420@gmail.com>
2023-07-05 12:12:07 +0530

    Added Parameterized and Non-Parameterized Constructor

---
+++ PracticeProblem3B/ParameterizedConstructor.cs
@@ -0,0 +1,21 @@
+﻿namespace PracticeProblem3B
+{
+    internal class ParameterizedConstructor
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        // Parameterized constructor
+        public ParameterizedConstructor(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public ParameterizedConstructor()
+        {
+            Name = "Vivek Verma";
+            Age = 24;
+        }
+    }
+}
---
+++ PracticeProblem3B/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PracticeProblem3B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ParameterizedConstructor person = new ParameterizedConstructor("Vivek Verma", 23);
+            Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+            ParameterizedConstructor p = new ParameterizedConstructor();
+            Console.WriteLine($"Name: {p.Name}, Age: {p.Age}");
+            Console.ReadLine();
+        }
+    }
+}
