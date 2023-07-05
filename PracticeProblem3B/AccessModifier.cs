de171f23a3344c3176122a076a5fbac706bbf700
Vivek Verma <vverma240420@gmail.com>
2023-07-05 13:25:27 +0530

    Added Access Modifier concept

---
+++ PracticeProblem3B/AccessModifier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem3B
+{
+    internal class AccessModifier
+    {
+        private int privateField;
+        protected int protectedField;
+        internal int internalField;
+        protected internal int protectedInternalField;
+        public int publicField;
+
+        private void PrivateMethod()
+        {
+            Console.WriteLine("Private method");
+        }
+
+        protected void ProtectedMethod()
+        {
+            Console.WriteLine("Protected method");
+        }
+
+        internal void InternalMethod()
+        {
+            Console.WriteLine("Internal method");
+        }
+
+        protected internal void ProtectedInternalMethod()
+        {
+            Console.WriteLine("Protected Internal method");
+        }
+
+        public void PublicMethod()
+        {
+            Console.WriteLine("Public method");
+        }
+    }
+}
--- PracticeProblem3B/Program.cs
+++ PracticeProblem3B/Program.cs
@@ -12,11 +12,25 @@ namespace PracticeProblem3B
             Console.WriteLine($"Name: {p.Name}, Age: {p.Age}"); */
 
 
-            IPlayable player1 = new MusicPlayer();
+           /* IPlayable player1 = new MusicPlayer();
             player1.Play();  
 
             IPlayable player2 = new VideoPlayer();
-            player2.Play();  
+            player2.Play();   */
+
+
+            AccessModifier ac = new AccessModifier();
+          //  ac.publicField = 10;                   // Accessible
+            ac.ProtectedInternalMethod();         // Accessible
+            ac.PublicMethod();                    // Accessible
+
+
+            //ac.privateField
+            // Not accessible
+            // ac.protectedField                    // Not accessible
+            // ac.internalField                     // Not accessible
+           // ac.ProtectedMethod();                // Not accessible
+            //ac.InternalMethod();                  // Not accessible
             Console.ReadLine();
         }
     }
