590b50bc4ace231a9e544f2c3b9762f564d1d8d3
Vivek Verma <vverma240420@gmail.com>
2023-07-05 13:00:27 +0530

    Added interface concept

---
+++ PracticeProblem3B/Interface.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem3B
+{
+    // Interface declaration
+    public interface IPlayable
+    {
+        void Play();
+    }
+
+    // Class implementing the interface
+    public class MusicPlayer : IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing music...");
+        }
+    }
+
+    // Class implementing the interface
+    public class VideoPlayer : IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing video...");
+        }
+    }
+
+   
+
+}
--- PracticeProblem3B/Program.cs
+++ PracticeProblem3B/Program.cs
@@ -6,10 +6,17 @@ namespace PracticeProblem3B
     {
         static void Main(string[] args)
         {
-            ParameterizedConstructor person = new ParameterizedConstructor("Vivek Verma", 23);
+           /* ParameterizedConstructor person = new ParameterizedConstructor("Vivek Verma", 23);
             Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
             ParameterizedConstructor p = new ParameterizedConstructor();
-            Console.WriteLine($"Name: {p.Name}, Age: {p.Age}");
+            Console.WriteLine($"Name: {p.Name}, Age: {p.Age}"); */
+
+
+            IPlayable player1 = new MusicPlayer();
+            player1.Play();  
+
+            IPlayable player2 = new VideoPlayer();
+            player2.Play();  
             Console.ReadLine();
         }
     }
